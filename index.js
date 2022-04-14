f6a6410456454fb65963ecf989c1ff5a66f0b8a1
suhwa <user@userui-MacBookAir.local>
2022-04-14 11:22:20 +0900

    eslint, prettier setting

---
+++ index.js
@@ -0,0 +1,14 @@
+// test code
+function init() {
+    const not_camel = "hello world";
+    const NEVER_USED = "never used";
+    console.log(`hello javascript!`);
+}
+init();
+
+// 구축한 방법
+// npm init : package.json(프로젝트 정보 저장)
+// npm i eslint@7 ~~ : 필요한 모듈 설치
+// eslintrc.json, prettierrc 설정 파일 추가
+
+// cypress.io 추가한 환경 구축 **
